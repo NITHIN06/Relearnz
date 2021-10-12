d958a3af6f8f63d1a45fb7981f0d5162253fd9b4
AkhilSai13 <akhilsaibande@gmail.com>
2021-10-12 12:02:12 +0530

    Added spinner to login and signup pages

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from kivymd.app import MDApp
+from kivy.lang import Builder
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.core.window import Window
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
+from kivymd.uix.spinner import MDSpinner
 from kivy.uix.screenmanager import ScreenManager, Screen 
+
 
 Window.size = (870, 580)
 Window.minimum_width, Window.minimum_height = Window.size
@@ -20,6 +23,7 @@ class Welcome(Screen):
 
 class Login(Screen):
     a = 0
+    dialog=None
     def logger(self):
         username = self.ids.user.text
         password = self.ids.password.text
@@ -32,10 +36,20 @@ class Login(Screen):
             self.ids.password.text = "FIELD SHOULD NOT BE EMPTY"
             self.a =0
 
-        if(self.a==1):
-            self.manager.current= "Tdashboard"
-            self.manager.transition.direction = "left"
-            return 
+        if(self.a==1): 
+
+            self.ids.load.active=True
+            if(1): # search for inputs in teacher database 
+                self.manager.current= "Tdashboard"
+                self.manager.transition.direction = "left"
+
+            elif(1): # search for inputs in student database 
+                self.manager.current= "Sdashboard"
+                self.manager.transition.direction = "left"
+            else:
+                self.dialog = MDDialog(title="Invalid Login", text="Enter correct credentials or signup if you don't have an account", radius=[20, 7, 20, 7])
+                self.dialog.open()
+            self.ids.load.active=False
         self.a=1    
 
 
@@ -67,18 +81,28 @@ class Register(Screen):
             self.a =0
 
         if( self.a ==1 and self.t==0):
-            self.dialog = MDDialog(title="Select a Role")
+            self.dialog = MDDialog(title="Select a Role", text="Please select either Teacher or Student role", radius=[20, 7, 20, 7])
             self.dialog.open()
             return 
 
         if( self.a ==1 and self.t==1):
+            self.ids.load.active=True
+
+            # store the values in the database
+
             self.manager.current= "Tdashboard"
             self.manager.transition.direction = "left"
+            self.ids.load.active=False
             return 
 
         if( self.a ==1 and self.t==-1):
+            self.ids.load.active=True
+
+            # store the values in the database   
+         
             self.manager.current= "Sdashboard"
             self.manager.transition.direction = "left"
+            self.ids.load.active=False
             return 
         self.a=1
 
